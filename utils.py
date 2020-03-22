337581b361d68833caaebdf2080294fc2f3ae88f
zhulei <leifzhu@foxmail.com>
2020-03-23 00:10:30 +0800

    add methodology.md

--- utils.py
+++ utils.py
@@ -80,24 +80,36 @@ def draw_bbs_to_img(im, bbs=None, bb_format='xywh_mat', mode='show'):
         return im_with_bb
     
     
-def get_row(items):
+
+def get_table_row(items):
     row = ''
     for item in items:
         row += '| {:s} '.format(item)
     row += '|\n'
     return row
 
-def generate_table(item_list, title=None, col_num=4):
+def generate_md_table(item_list, title=None, col_num=4):
+    '''
+    args: 
+        item_list: a list of items (str or can be converted to str)
+        that want to be presented in table.
+        
+        title: None, or a list of strings. When set to None, empty title
+        row is used and column number is determined by col_num; Otherwise, 
+        it will be used as title row, its length will override col_num.
+    
+    return: 
+        table: markdown table string.
+    '''
     table = ''
-    if title is None:
-        title = [ 'item_{:d}'.format(i) for i in range(1, col_num+1) ]
-    else:
+    if title is not None:
         col_num = len(title)
-    table += get_row(title)
-    table += get_row(['-'] * col_num)
+        table += get_table_row(title)
+    else:
+        table += get_table_row([' ']*col_num) # empty title row
+    table += get_table_row(['-'] * col_num)
     for i in range(0, len(item_list), col_num):
-        table += get_row(item_list[i:i+col_num])
+        table += get_table_row(item_list[i:i+col_num])
     return table
-
     
     
